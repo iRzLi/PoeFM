2b65d85391e50d7bb6441294ca10c908fe855495
Run Zhuo Li <runzhuoli.cs@gmail.com>
2019-08-05 16:34:26 -0400

    add the static routes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const items = require("./routes/api/items");
+const stats = require("./routes/api/stats");
+const static = require("./routes/api/static");
 
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/items", items);
+app.use("/api/stats", stats);
+app.use("/api/static", static);
 
 const port = process.env.PORT || 5000;
 
---
+++ routes/api/static.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const request = require('request');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    request("https://www.pathofexile.com/api/trade/data/static", (err, resp, body) => {
+        if (!err && resp.statusCode == 200) {
+            return res.json(JSON.parse(body)["result"]);
+        }
+    });
+});
+
+module.exports = router;
